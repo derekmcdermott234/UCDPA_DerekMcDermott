dce6199d1b09cb4d797ef88507a3e04d59f17c67
derekmcdermott234 <derekmcdermott234@gmail.com>
2021-09-07 22:55:55 +0100

    Importing 2 separate S&P 500 files into dataframe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 sp500_0=pd.read_csv('C:/Users/derek/Desktop/ucd/data/all_stocks_5yr_0.csv')
 sp500_1=pd.read_csv('C:/Users/derek/Desktop/ucd/data/all_stocks_5yr_1.csv')
 #sp500_0=pd.read_csv('https://github.com/derekmcdermott234/UCDPA_DerekMcDermott/upload/master/all_stocks_5yr_0.csv')
@@ -7,10 +8,10 @@ print('...............')
 print(sp500_0.shape)
 print(sp500_0.head(5))
 missing_values_count_0=sp500_0.isnull().sum()
-print(missing_values_count_0)
+print(missing_values_count_0[:])
 print('...............')
 print(sp500_1.shape)
 print(sp500_1.head(5))
 missing_values_count_1=sp500_1.isnull().sum()
-print(missing_values_count_1)
+print(missing_values_count_1[:])
 
