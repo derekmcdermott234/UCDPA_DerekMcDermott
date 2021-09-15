bf390971b7d9eebab7fcb0a394437e687acd86a7
derekmcdermott234 <derekmcdermott234@gmail.com>
2021-09-15 16:45:50 +0100

    Nestle switzerland stock price simulation from 17 august 2021 for 10 days using monte carlo methods. Simulated stock price trajectory averaged over 1000 runs

--- nestle_sim.py
+++ nestle_sim.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-fig,ax=plt.subplots(2,2)
+fig,ax=plt.subplots(3,3)
 nestle=pd.read_csv('C:/Users/derek/Desktop/ucd/Working Data/yahoo data/NESN.SW.csv',
                    parse_dates=['Date'],index_col='Date')
 print(nestle.shape)
@@ -49,18 +49,28 @@ c_neww=neww.drop("new_col",axis=1)
 c_neww['MEAN']=c_neww.mean(axis=1)
 print(c_neww['MEAN'].head())
 print(c_neww.info())
-
-list_1=c_neww.mean(axis=1)
 print('...')
 print(neww.info())
 print(neww)
-print(neww['new_col'])
 print('...')
-sim_1=neww.iloc[:,1:2];sim_2=neww.iloc[:,2:3];sim_3=neww.iloc[:,3:4];sim_4=neww.iloc[:,4:5]
-sim_5=neww.iloc[:,5:6];sim_6=neww.iloc[:,6:7];sim_7=neww.iloc[:,7:8];sim_8=neww.iloc[:,8:9]
+sim_2=neww.iloc[:,1:2];sim_3=neww.iloc[:,2:3];sim_4=neww.iloc[:,3:4];sim_5=neww.iloc[:,4:5]
+sim_6=neww.iloc[:,5:6];sim_7=neww.iloc[:,6:7];sim_8=neww.iloc[:,7:8];sim_9=neww.iloc[:,8:9]
 ax[0][0].plot(neww['new_col'],color='b')
 ax[0][0].plot(c_neww['MEAN'],color='r')
-
-#ax[0][0].plot(neww['new_col'],color='b')
-#ax[0][1].plot(sim_5,color='r')
+ax[0][1].plot(neww['new_col'],color='b')
+ax[0][1].plot(sim_2,color='r')
+ax[0][2].plot(neww['new_col'],color='b')
+ax[0][2].plot(sim_3,color='r')
+ax[1][0].plot(neww['new_col'],color='b')
+ax[1][0].plot(sim_4,color='r')
+ax[1][1].plot(neww['new_col'],color='b')
+ax[1][1].plot(sim_5,color='r')
+ax[1][2].plot(neww['new_col'],color='b')
+ax[1][2].plot(sim_6,color='r')
+ax[2][0].plot(neww['new_col'],color='b')
+ax[2][0].plot(sim_7,color='r')
+ax[2][1].plot(neww['new_col'],color='b')
+ax[2][1].plot(sim_8,color='r')
+ax[2][2].plot(neww['new_col'],color='b')
+ax[2][2].plot(sim_9,color='r')
 plt.show()
