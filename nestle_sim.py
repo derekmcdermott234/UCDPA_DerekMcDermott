63f4f5f0c1628e01b4449c8a93ed752338a29adc
derekmcdermott234 <derekmcdermott234@gmail.com>
2021-09-15 17:59:42 +0100

    Nestle switzerland stock price simulation from 17 august 2021 for 10 days using monte carlo methods. Simulated stock price trajectory averaged over 1000 runs

--- nestle_sim.py
+++ nestle_sim.py
@@ -23,30 +23,29 @@ print(nestle_daily_returns.head())
 z=0
 j=0
 price_17aug2021=nestle_close_price[2728]
-set_price=[price_17aug2021,0,0,0,0,0,0,0,0,0]
+set_price=[price_17aug2021,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 mu = nestle_daily_returns[:2729].mean() #to 17/08/2021
 standard_dev=nestle_daily_returns[:2729].std()
 print('...')
 print(mu,standard_dev)
 print('...')
-Index_1=[0,1,2,3,4,5,6,7,8,9]
+Index_1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 neww=pd.DataFrame()
 neww['Index']=Index_1
 neww=neww.set_index('Index')
-print(nestle_close_price[2728])
-neww['new_col'] = np.array(nestle_close_price.iloc[2728:2738])
-phi= np.random.normal(loc=mu, scale=standard_dev, size=(1,10000))
-for l in range(0,1000):
+neww['new_col'] = np.array(nestle_close_price.iloc[2728:2748])
+phi= np.random.normal(loc=mu, scale=standard_dev, size=(1,1000))
+for l in range(0,100):
    y = price_17aug2021
    prices_1=set_price
-   for k in range(0,9):
+   for k in range(0,19):
       y = y+y*(phi[0][l+k])
       prices_1[k+1]=y
-   print(prices_1)
    d_series=pd.Series(prices_1)
    neww=pd.concat([neww,d_series],axis=1)
 c_neww=neww.drop("new_col",axis=1)
 c_neww['MEAN']=c_neww.mean(axis=1)
+c_neww['MEAN'].to_csv('C:/Users/derek/Desktop/ucd/Working Data/yahoo data/nestle_mean_columns.csv',mode='a', index=True, header=False)
 print(c_neww['MEAN'].head())
 print(c_neww.info())
 print('...')
@@ -74,3 +73,4 @@ ax[2][1].plot(sim_8,color='r')
 ax[2][2].plot(neww['new_col'],color='b')
 ax[2][2].plot(sim_9,color='r')
 plt.show()
+print(neww['new_col'])
