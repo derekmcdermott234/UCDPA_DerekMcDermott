d579957e3634276607fc77b91bfe3792f74d610e
derekmcdermott234 <derekmcdermott234@gmail.com>
2021-09-22 17:32:30 +0100

    Amazon price simulations  for 43 days

--- AMAZON_Long run.py
+++ AMAZON_Long run.py
@@ -22,7 +22,7 @@ print('The price on 17/09/2021 is: ', price_17sep2021)
 set_price=np.array([price_17sep2021,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]) #create a constant list of 20 values to be referenced
 print                                                               #within the loop structure to follow.#in one run generate 20 simulated closing prices from 18/08/2021 onwards and add this singe run as a column of data
 
-for l in range(0,2500):    #100 simulations will be run
+for l in range(0,1500):    #100 simulations will be run
    y = price_17sep2021    #reset y to the close price on 18/aug/2021
    prices_1=set_price     #reset prices_1 array before it accepts new values from the k indexed loop
    for k in range(0,43):
@@ -30,6 +30,11 @@ for l in range(0,2500):    #100 simulations will be run
       prices_1[k+1]=y       #store the next day's simulated closing price in a list
    d_series=pd.Series(prices_1) #convert the list to a series before you can add it to neww
    neww=pd.concat([neww,d_series],axis=1)
+quantile_25=neww.quantile(.25,axis=1)
+quantile_40=neww.quantile(.40,axis=1)
+quantile_45=neww.quantile(.45,axis=1)
+quantile_55=neww.quantile(.55,axis=1)
+quantile_60=neww.quantile(.60,axis=1)
 median_1=neww.median(axis=1)
 mean_1=neww.mean(axis=1).tolist()
 std_1=neww.std(axis=1).tolist()
@@ -41,14 +46,20 @@ mean_plus_2std_array=mean_1_array+2*std_1_array
 mean_minus_2std_array=mean_1_array-2*std_1_array
 fig,ax=plt.subplots()
 
-plt.plot(median_1,color='b')
-plt.plot(mean_1_array,color='c')
-plt.plot(mean_plus_std_array,color='r')
+plt.plot(median_1,color='c',label='Median trend line')
+plt.plot(mean_1_array,color='b',label='Mean trend line')
+plt.plot(mean_plus_std_array,color='r',label='68% confidence interval')
 plt.plot(mean_minus_std_array,color='r')
-plt.plot(mean_plus_2std_array,color='r')
-plt.plot(mean_minus_2std_array,color='r')
-
-plt.plot(AMAZ_CLOSE,color='y')
+plt.plot(mean_plus_2std_array,color='m',label='95% confidence interval')
+plt.plot(mean_minus_2std_array,color='m')
+plt.title('43 day simulated run of Amazon share price from 18/08/2021 on onwards ')
+plt.plot(AMAZ_CLOSE,color='y',label='Actual Amazon Close price')
+plt.plot(quantile_45,color='g',label='quantile.45')
+plt.plot(quantile_55,color='g',label='quantile.55')
+plt.plot(quantile_60,color='g',label='quantile.60')
+plt.plot(quantile_40,color='g',label='quantile.40')
+plt.plot(quantile_25,color='g',label='quantile.25')
+plt.legend()
 #plt.plot(neww.iloc[:,0:1],color='r')
 
 #---------
