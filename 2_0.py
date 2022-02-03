92fc7f7d3771eeadc5c02e9f6295e83de90d77fd
derekmcdermott234 <derekmcdermott234@gmail.com>
2022-02-03 22:22:18 +0000

    Amazon price simulations  for 43 days

---
+++ 2_0.py
@@ -0,0 +1,9 @@
+import codeop
+import matplotlib.pyplot as plt #Import the matplotlib.ptplot libary
+import seaborn as sns           #Import the Seaborn libary for plotting
+import datetime as datetime     #Import the datetime libary for display
+from fitter import Fitter, get_common_distributions, get_distributions  #fitter package provides a simple class to
+                                                                        # identify the distribution from which a data
+                                                                        #samples is generated fromimport yfinance as yfimport numpy as np
+import openpyxl
+import maincode2_0
